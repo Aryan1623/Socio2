9b50721016868313e220c2b0c72cfa07de248577
Aryan1623 <147040123+Aryan1623@users.noreply.github.com>
2025-01-08 14:17:53 +0530

    Update urls.py

--- socioproj/window/urls.py
+++ socioproj/window/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # Default index view
+    path('', views.index, name='index'),
+
+    # View for chat.html
+    path('chat/', views.chat_view, name='chat'),
+
+    # View for graphs.html
+    path('graphs/', views.graph_view, name='graphs'),
 ]
+
